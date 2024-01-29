048a49f2fa4153b9865b02f2ea885672cbd04e1a
Prashant Sonawane <Prashant.Sonawane@harbingergroup.com>
2024-01-29 17:38:32 +0530

    index file add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Greet from './componets/Greet';
+//import index from './componets/index';
 function App() {
   return (
     <div className="App">
---
+++ src/componets/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const index = () => {
+    return (
+        <div>hello we are learning react.js</div>
+    )
+}
+export default index
